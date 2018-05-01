4226f98080ef267ff082f503d5cadcea3953279f
Donghyeon Kim <donghyeon@tacocube.com>
2018-05-01 09:45:12 -0400

    Modification

--- analyze.py
+++ analyze.py
@@ -22,16 +22,6 @@ def parse_bar(df):
     return df.drop(['Bar'], axis=1)
 
 
-def parse_section_by_row(row):
-    m = section_format.search(row['Section'])
-    return float(m.group('b')), float(m.group('h'))
-
-
-def parse_section(df):
-    df['B'], df['H'] = zip(*df.apply(parse_section_by_row, axis=1))
-    return df.drop(['Section'], axis=1)
-
-
 def unit_conversion(df):
     # TODO Implement.
     return df.rename(columns={
@@ -40,7 +30,10 @@ def unit_conversion(df):
         'FZ (kip)': 'FZ',
         'MX (kip-ft)': 'MX',
         'MY (kip-ft)': 'MY',
-        'MZ (kip-ft)': 'MZ'})
+        'MZ (kip-ft)': 'MZ',
+        'Length (ft)': 'L',
+        'RECT_BF (in)': 'B',
+        'RECT_HT (in)': 'H'})
 
 
 def distinguish_minmax_by_row(row):
@@ -89,7 +82,7 @@ def distinguish_minmax(df):
     return df.drop(['MINMAX', 'FX', 'FY', 'FZ', 'MX', 'MY', 'MZ'], axis=1)
 
 
-def calculate_required_reinforcement_by_row(row):
+def beam_calculate_required_reinforcement_by_row(row):
     print('Required reinforcement [bar #%d]' % row['ID'])
     area_top = numpy.nan
     area_bottom = numpy.nan
@@ -109,29 +102,30 @@ def calculate_required_reinforcement_by_row(row):
     return area_top, area_bottom
 
 
-def calculate_required_reinforcement(df):
+def beam_calculate_required_reinforcement(df):
     # Calculate required reinforcement area in parallel.
     # Number of processes generated is equal to cpu count by default.
     with Pool() as p:
-        df['AREA TOP'], df['AREA BOTTOM'] = zip(*p.map(calculate_required_reinforcement_by_row,
-                                                       df.T.to_dict().values()))
+        df = df.assign(**{'AREA TOP': numpy.nan, 'AREA BOTTOM': numpy.nan})
+        df.loc[:,['AREA TOP','AREA BOTTOM']] = p.map(beam_calculate_required_reinforcement_by_row,
+                                                   df.T.to_dict().values())
     return df
 
 
-def calculate_stirrup_spacing_by_row(row):
+def beam_calculate_stirrup_spacing_by_row(row):
     print('Stirrup spacing [bar #%d]' % row['ID'])
     case1 = beam.shear_spacing(row['B'], row['H']-C_C, row['H'], row['MAX FX'], row['MAX FZ'])
     case2 = beam.shear_spacing(row['B'], row['H']-C_C, row['H'], row['MIN FX'], row['MIN FZ'])
     return min(case1, case2)
 
 
-def calculate_stirrup_spacing(df):
+def beam_calculate_stirrup_spacing(df):
     with Pool() as p:
-        df['STIRRUP SPACING'] = p.map(calculate_stirrup_spacing_by_row, df.T.to_dict().values())
+        df.insert(len(df.columns), 'STIRRUP SPACING', p.map(beam_calculate_stirrup_spacing_by_row, df.T.to_dict().values()))
     return df
 
 
-def design_reinforcement_by_row(row):
+def beam_design_reinforcement_by_row(row):
     print('Reinforcement design [bar #%d]' % row['ID'])
     if row['AREA TOP'] is numpy.nan or row['AREA BOTTOM'] is numpy.nan:
         # If required area calculation has failed, put minimum reinforcement.
@@ -145,16 +139,16 @@ def design_reinforcement_by_row(row):
            bottom['bar'], bottom['count'], rebar.area[bottom['bar']]*bottom['count']
 
 
-def design_reinforcement(df):
+def beam_design_reinforcement(df):
     # Determine number and type of rebars.
     with Pool() as p:
-        df['BAR TYPE TOP'], df['BAR COUNT TOP'], df['AREA TOP'],\
-        df['BAR TYPE BOTTOM'], df['BAR COUNT BOTTOM'], df['AREA BOTTOM']\
-        = zip(*p.map(design_reinforcement_by_row, df.T.to_dict().values()))
+        df['BAR TYPE TOP'],df['BAR COUNT TOP'],df['AREA TOP'],\
+        df['BAR TYPE BOTTOM'],df['BAR COUNT BOTTOM'],df['AREA BOTTOM']\
+        = zip(*p.map(beam_design_reinforcement_by_row, df.T.to_dict().values()))
     return df
 
 
-def check_required_strength_by_row(row):
+def beam_check_required_strength_by_row(row):
     print('Required strength check [bar #%d]' % row['ID'])
     # Check positive moment case and negative moment case.
     efficiency_max_my = 1
@@ -177,29 +171,38 @@ def check_required_strength_by_row(row):
     return efficiency_min_my, efficiency_max_my
 
 
-def check_required_strength(df):
+def beam_check_required_strength(df):
     with Pool() as p:
-        df['EFFICIENCY MIN MY'], df['EFFICIENCY MAX MY'] = zip(*p.map(check_required_strength_by_row,
-                                                                      df.T.to_dict().values()))
+        df['EFFICIENCY MIN MY'],df['EFFICIENCY MAX MY'] = zip(*p.map(beam_check_required_strength_by_row,
+                                                              df.T.to_dict().values()))
     return df
 
 
-def process_data(df):
-    df = parse_bar(df)
-    df = parse_section(df)
-    df = unit_conversion(df)
-    df = distinguish_minmax(df)
-    df = df.groupby(['ID']).first().reset_index()
-    df = calculate_required_reinforcement(df)
-    df = calculate_stirrup_spacing(df)
-    df = design_reinforcement(df)
-    df = check_required_strength(df)
-    df = df.groupby(['ID']).first()
-    print(df)
+def process_data(all):
+    all = parse_bar(all)
+    all = unit_conversion(all)
+    all = distinguish_minmax(all)
+    all = all.groupby(['ID']).first().reset_index()
+
+    # Process beam forces.
+    beam_dataframe = all[(all['Type'] == 'RC Beam')]
+    beam_dataframe = beam_calculate_required_reinforcement(beam_dataframe)
+    beam_dataframe = beam_calculate_stirrup_spacing(beam_dataframe)
+    beam_dataframe = beam_design_reinforcement(beam_dataframe)
+    beam_dataframe = beam_check_required_strength(beam_dataframe)
+    beam_dataframe = beam_dataframe.groupby(['ID']).first()
+    print(beam_dataframe)
+
+    # Process column forces.
+    column_dataframe = all[(all['Type'] == 'RC Column')]
+    column_dataframe = column_dataframe.groupby(['ID']).first()
+    print(column_dataframe)
 
     # Write result to output file.
-    df.to_csv('output.csv')
+    beam_dataframe.to_csv('beam.csv')
+    column_dataframe.to_csv('column.csv')
 
 if __name__ == "__main__":
     # Process csv file.
-    process_data(pandas.read_csv('data/forces.csv'))
+    process_data(pandas.read_csv('data/forces.csv')[522:600])
+    print('Analysis complete')
